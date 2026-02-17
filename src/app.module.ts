dd52dd8ee82388944fc117027c962ab7c7436b4d
Your Name~ <you@example.com>
2026-02-17 15:46:22 +0500

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoModule } from './todo/todo.module';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [PrismaModule, TodoModule, PostModule],
+  imports: [PrismaModule, TodoModule, PostModule, UserModule, CommentModule, TagModule],
   controllers: [],
   providers: [],
 })

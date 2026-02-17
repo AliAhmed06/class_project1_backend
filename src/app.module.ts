f11d0e744ca1a84d9cf7511f9b613ac987815fcd
Your Name~ <you@example.com>
2026-02-17 13:13:10 +0500

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoModule } from './todo/todo.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
-    }),
-    PrismaModule,
-    TodoModule,
-    PostModule,
-  ],
+  imports: [PrismaModule, TodoModule, PostModule],
   controllers: [],
   providers: [],
 })
